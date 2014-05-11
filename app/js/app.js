04cdbff61cc994309b2f1a339528f2945cbdbf5f
Luke Bakker <lukebakker000@gmail.com>
2014-05-11 20:33:14 +1000

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+// // Declare app level module which depends on filters, and services
+// angular.module('myApp', [
+//   'ngRoute',
+//   'myApp.filters',
+//   'myApp.services',
+//   'myApp.directives',
+//   'myApp.controllers'
+// ]).
+// config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
+angular.module('plunker', ['ui.bootstrap']);
+function AccordionDemoCtrl($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+}
